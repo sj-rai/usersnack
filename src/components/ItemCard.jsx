aba2b39ff07900d24888e1843d0e8bdcbddc2b01
Sanjoth Rai <sanjr4279@gmail.com>
2022-07-21 12:18:12 +0530

    render data from file

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,9 +1,18 @@
 import './ItemCard.css'
 
-export function ItemCard() {
+export function ItemCard({children}) {
+    let {name, price, ingredients, img} = children;
     return (
         <div className="card">
-            Item
+            <h2>{name}</h2>
+            {ingredients && ingredients.map((ingredient) => {
+                if(ingredients.indexOf(ingredient) === 0) {
+                    return <span>{ingredient}</span>
+                } else {
+                    return <span>, {ingredient}</span>
+                }
+            })}
+            <h3>USD {price}</h3>
         </div>
     )
 }
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,9 +1,42 @@
+import React, { useEffect, useState } from "react"
 import { ItemCard } from "./ItemCard"
+
 export function List() {
+    const [pizzaList, setPizzaList] = useState([])
+
+    useEffect(() => {
+        fetchData().then((data) => {
+            console.log('[data]', data)
+            setPizzaList(data.data.Pizza)
+        }).catch((err) => {
+            console.log('[err]', err)
+        })
+    }, [])
+
     return (
         <ul>
-            <li><ItemCard /></li>
-            <li><ItemCard /></li>
+        {pizzaList && pizzaList.map((item, key) => {
+            return (<li key={key}>
+                        <ItemCard>{item}</ItemCard>
+                    </li>)
+            })}
         </ul>
     )
+}
+
+function fetchData() {
+    return new Promise((resolve, reject) => {
+        return fetch('data/data.json').then((response) => {
+            console.log('[response]', response)
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response.json()
+        }).then((data) => {
+            console.log("[data]", data)
+            resolve({data: data})
+        }).catch(err => {
+            reject(err);
+        });
+    })
 }
