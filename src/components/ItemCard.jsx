ab0263b69c0f829d1642b13f53bd8afa676703ce
Sanjoth Rai <sanjr4279@gmail.com>
2022-07-21 15:16:20 +0530

    image and media query

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -4,15 +4,22 @@ export function ItemCard({children}) {
     let {name, price, ingredients, img} = children;
     return (
         <div className="card">
-            <h2>{name}</h2>
-            {ingredients && ingredients.map((ingredient) => {
-                if(ingredients.indexOf(ingredient) === 0) {
-                    return <span>{ingredient}</span>
-                } else {
-                    return <span>, {ingredient}</span>
-                }
-            })}
-            <h3>USD {price}</h3>
+            <div className="details">
+                <h2>{name}</h2>
+                {ingredients && ingredients.map((ingredient) => {
+                    if(ingredients.indexOf(ingredient) === 0) {
+                        return <span>{ingredient}</span>
+                    } else {
+                        return <span>, {ingredient}</span>
+                    }
+                })}
+                <div className="price">
+                    <h3>USD {price}</h3>
+                </div>
+            </div>
+            <div className="image">
+                <img src={`data/img/${img}`}/>
+            </div>
         </div>
     )
 }
